c691c9fb273c6ae6c8eb54e9ab3a736c013b4ce7
Geir Kvenseth <guggen33@gmail.com>
2018-02-12 16:54:13 +0100

    Gjort nesten ferdi

--- Newspaper.java
+++ Newspaper.java
@@ -3,8 +3,8 @@
  * Store details of a newspaper,
  * such as name, date/year, pages, newspaperNumber, vintage
  *
- * @author (Geir Kvenseth)
- * @version (a version number or a date)
+ * @author Geir Kvenseth, Fride frøland, Helene Rasmussen
+ * @version (12.02.2018)
  */
 public class Newspaper
 {
@@ -24,7 +24,7 @@ public class Newspaper
      * @param numberOfPages Number of pages in the newspaper.
      */
     public Newspaper(String name, int issueNumber, 
-                        int vintage, String date, int numberOfPages)
+    int vintage, String date, int numberOfPages)
     {
         this.name = name;
         this.issueNumber = issueNumber;
@@ -39,38 +39,103 @@ public class Newspaper
      * @param issueNumber The number of the paper.
      * @param dateAndYear Date and year published.
      */
-        public Newspaper(String name, int issueNumber, 
-                        String date)
+    public Newspaper(String name, int issueNumber, 
+    String date)
     {
         this.name = name;
         this.issueNumber = issueNumber;
+        this.vintage = 0;
+        this.date = date;
+        this.numberOfPages = 0;
+    }
+
+    /**
+     * Returns the name.
+     * @returns name The name of the paper.
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * Returns the issueNumber.
+     * @returns issueNumber The Newspaper number.
+     */
+    public int getIssueNumber()
+    {
+        return this.issueNumber;
+    }
+    
+    /**
+     * Returns vintage.
+     * @returns vintage The Vintage.
+     */
+    public int getVintage()
+    {
+        return this.vintage;
+    }
+    
+    /**
+     * Return date.
+     * @return date The newspaper date.
+     */
+    public String getDate()
+    {
+        return this.date;
+    }
+    
+    /**
+     * Return number of pages in the newspaper.
+     * @return numberOfPages The number of pages.
+     */
+    public int getNumberOfPages()
+    {
+        return this.numberOfPages;
+    }
+    
+    /**
+     * Sets the name of the paper.
+     * @param name The name of the paper.
+     */
+    private void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    /**
+     * Sets the issuenumber.
+     * @param issuenumber The issue number.
+     */
+    private void setIssueNumber(int issueNumber)
+    {
+        this.issueNumber = issueNumber;
+    }
+    
+    /**
+     * Sets the vintage.
+     * @param vintage The vintage of the paper. 
+     */
+    private void setVintage(int vintage)
+    {
+        this.vintage = vintage;
+    }
+    
+    /**
+     * Sets the date.
+     * @param date The date of the paper.
+     */
+    private void setDate(String date)
+    {
         this.date = date;
     }
     
-     /**
-     * Sets all information about the newspaper.
-     * @param name The newspapers name.
-     * @param issueNumber The number of the paper.
-     * @param vintage Total year number since first published.
-     * @param dateAndYear Date and year published.
-     * @param numberOfPages Number of pages in the newspaper.
-     */
-    private void setAllParam(String name, int issueNumber, 
-                        int vintage, String date, int numberOfPages)
-    {
-        this.name = name;
-        this.issueNumber = issueNumber;
-        this.vintage = vintage;
-        this.date = date;
-        this.numberOfPages = numberOfPages;
-    }
-        
     /**
-     * Return name
-     * @return the name of the paper
+     * Sets the number of pages.
+     * @param numberOfPages The number of pages.
      */
-    private String getname()
+    private void setNumberOfPages(int numberOfPages)
     {
-        return this.name;
+        this.numberOfPages = numberOfPages;
     }
 }
--- NewspaperOrganizer.java
+++ NewspaperOrganizer.java
@@ -1,34 +1,58 @@
 import java.util.ArrayList;
 /**
- * Write a description of class NewspaperOrganizer here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Represents a collections of newspapers.
+ * @author Geir Kvenseth, Fride frøland, Helene Rasmussen
+ * @version (12.02.2018)
  */
 public class NewspaperOrganizer
 {
     // An Arraylist for storing objects of class Newspaper.
-    private ArrayList<newsPaper> newsPapers;
-    // 
-
+    private ArrayList<Newspaper> listOfNewsPapers;
+    
     /**
      * Constructor for objects of class NewspaperOrganizer
      */
     public NewspaperOrganizer()
     {
-        // initialise instance variables
-        x = 0;
+        this.listOfNewsPapers = new ArrayList<Newspaper>();
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Adds a newspaper to the newspaperorganizer.
+     * @param newspaper The newspaper to add
      */
-    public int sampleMethod(int y)
+    public void addnewspaper(Newspaper newspaper)
     {
-        // put your code here
-        return x + y;
+        this.listOfNewsPapers.add(newspaper);
+    }
+    
+    /**
+     * 
+     */
+    public void removeNewspaper(int index)
+    
+    {
+        
+    }
+    
+    /**
+     * Find and return the newspaper with a name matching the parameter name.
+     * @param name The name of the newspaper to search for.
+     */
+    public Newspaper findNewspaperByName(String name)
+    {
+        Newspaper foundNewspaper = null;
+        int index = 0;
+
+        while ((null == foundNewspaper) && (index < this.listOfNewsPapers.size()))
+        {
+            Newspaper n = this.listOfNewsPapers.get(index++);
+            if (n.getName().equals(name))
+            {
+                foundNewspaper = n;  
+            }
+
+        }
+        return foundNewspaper;
     }
 }
