98dbcb4fa137470f0f51adefa035c0895e59b1db
Gugge <guggen33@gmail.com>
2018-02-12 14:59:29 +0100

    Litt av kvart

--- Newspaper.java
+++ Newspaper.java
@@ -3,7 +3,7 @@
  * Store details of a newspaper,
  * such as name, date/year, pages, newspaperNumber, vintage
  *
- * @author (your name)
+ * @author (Geir Kvenseth)
  * @version (a version number or a date)
  */
 public class Newspaper
@@ -26,24 +26,51 @@ public class Newspaper
     public Newspaper(String name, int issueNumber, 
                         int vintage, String date, int numberOfPages)
     {
-        this.newspaperName = newspaperName;
-        this.
+        this.name = name;
+        this.issueNumber = issueNumber;
+        this.vintage = vintage;
+        this.date = date;
+        this.numberOfPages = numberOfPages;
     }
 
-    
-    
-    
-    
-    
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Constructor for objects of class Newspaper
+     * @param name The newspapers name.
+     * @param issueNumber The number of the paper.
+     * @param dateAndYear Date and year published.
      */
-    public int sampleMethod(int y)
+        public Newspaper(String name, int issueNumber, 
+                        String date)
     {
-        // put your code here
-        return x + y;
+        this.name = name;
+        this.issueNumber = issueNumber;
+        this.date = date;
+    }
+    
+     /**
+     * Sets all information about the newspaper.
+     * @param name The newspapers name.
+     * @param issueNumber The number of the paper.
+     * @param vintage Total year number since first published.
+     * @param dateAndYear Date and year published.
+     * @param numberOfPages Number of pages in the newspaper.
+     */
+    private void setAllParam(String name, int issueNumber, 
+                        int vintage, String date, int numberOfPages)
+    {
+        this.name = name;
+        this.issueNumber = issueNumber;
+        this.vintage = vintage;
+        this.date = date;
+        this.numberOfPages = numberOfPages;
+    }
+        
+    /**
+     * Return name
+     * @return the name of the paper
+     */
+    private String getname()
+    {
+        return this.name;
     }
 }
